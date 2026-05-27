93466ddeee59b4a333433ab230b56eda9ad0f42d
Kiju Jeong <kijujeong.dev@gmail.com>
2026-05-27 16:32:42 +0900

    init

---
+++ src/test/java/com/exchange/exchange_server/ExchangeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.exchange_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
